8551e68ce462fdae5fa138cb26aaca7bd1083735
Charlie Harding <xsanda@me.com>
2019-06-19 12:18:25 +0200

    Add support for nodes and arrays of children

--- tag.js
+++ tag.js
@@ -1,21 +1,33 @@
 // Jonas Karg 2018
+(() => {
+    const isString = x => typeof x === 'string' || x instanceof String;
+    const isNumber = x => typeof x === 'number' || x instanceof Number;
+    const isBoolean = x => typeof x === 'boolean' || x instanceof Boolean;
+    const isNode = x => x instanceof HTMLElement;
+    const isIterable = x => Symbol.iterator in Object(x);
 
-["a", "abbr", "address", "area", "article", "aside", "audio", "b", "base", "bdi", "bdo", "blockquote", "body", "br", "button", "canvas", "caption", "center", "cite", "code", "col", "colgroup", "command", "datalist", "dd", "del", "details", "dfn", "div", "dl", "dt", "em", "embed", "fieldset", "figcaption", "figure", "footer", "form", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "hgroup", "hr", "html", "i", "iframe", "img", "input", "ins", "kbd", "label", "legend", "li", "link", "main", "map", "mark", "meta", "meter", "nav", "noscript", "object", "ol", "optgroup", "option", "output", "p", "param", "picture", "pre", "progress", "q", "rp", "rt", "ruby", "s", "samp", "script", "section", "select", "small", "source", "span", "strong", "style", "sub", "summary", "sup", "svg", "table", "tbody", "td", "template", "textarea", "tfoot", "th", "thead", "time", "title", "tr", "track", "u", "ul", "video", "wbr"].map(tag => {
-    window[tag] = function () {
-        let element = document.createElement(tag);
-        let elementArguments;
-        let elementInnerHTML;
-        let returnString;
+    const d = document;
 
-        Array.from(arguments).map(arg => {
-            if (arg.constructor === Boolean) returnString = arg;
-            else if (arg.constructor === String || arg.constructor === Number) elementInnerHTML = String(arg);
-            else if (arg.constructor === Object) elementArguments = arg;
-        });
+    const liftElement = el => isNode(el) ? el : d.createTextNode(el);
 
-        element.innerHTML = elementInnerHTML || "";
-        Object.assign(element, elementArguments || {});
-        if (returnString) return element.outerHTML;
-        return element;
-    }
-});
+    ["a", "abbr", "address", "area", "article", "aside", "audio", "b", "base", "bdi", "bdo", "blockquote", "body", "br", "button", "canvas", "caption", "center", "cite", "code", "col", "colgroup", "command", "datalist", "dd", "del", "details", "dfn", "div", "dl", "dt", "em", "embed", "fieldset", "figcaption", "figure", "footer", "form", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "hgroup", "hr", "html", "i", "iframe", "img", "input", "ins", "kbd", "label", "legend", "li", "link", "main", "map", "mark", "meta", "meter", "nav", "noscript", "object", "ol", "optgroup", "option", "output", "p", "param", "picture", "pre", "progress", "q", "rp", "rt", "ruby", "s", "samp", "script", "section", "select", "small", "source", "span", "strong", "style", "sub", "summary", "sup", "svg", "table", "tbody", "td", "template", "textarea", "tfoot", "th", "thead", "time", "title", "tr", "track", "u", "ul", "video", "wbr"].map(tag => {
+        window[tag] = (...args) => {
+            const element = d.createElement(tag);
+            let elementArguments;
+            let elementChildren = [];
+            let returnString = false;
+
+            args.forEach(arg => {
+                if (isBoolean(arg)) returnString = arg;
+                else if (isNode(arg) || isString(arg) || isNumber(arg)) elementChildren = [arg];
+                else if (isIterable(arg)) elementChildren = Array.from(arg);
+                else if (arg.constructor === Object) elementArguments = arg;
+            });
+
+            elementChildren.forEach(el => element.appendChild(liftElement(el)));
+            Object.assign(element, elementArguments || {});
+            if (returnString) return element.outerHTML;
+            return element;
+        }
+    });
+})();
